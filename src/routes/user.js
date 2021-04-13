7355f78b755e8047a9f72f30bcf41304cf21b50a
Shyam Padodara <padodarashyam@gmail.com>
2021-04-13 12:50:23 +0530

    uer rout /user/me added

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
     // Create new user (Normal user and Admin)
     app.post('/user', async (req, res) => {
         const user = new User(req.body)
-        
+
         try {
             await user.save();
             const token = await user.generateAuthToken()
@@ -30,18 +30,18 @@ module.exports = (app) => {
         try {
             req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
             await req.user.save()
-    
+
             res.send()
         } catch (e) {
             res.status(500).send()
         }
     })
-    
+
     app.post('/user/logoutAll', auth, async (req, res) => {
         try {
             req.user.tokens = []
             await req.user.save()
-    
+
             res.send()
         } catch (e) {
             res.status(500).send()
@@ -49,17 +49,33 @@ module.exports = (app) => {
     })
 
     // Get user profile
-    app.get('/user/me', auth, async  (req, res) => {
+    app.get('/user/me', auth, async (req, res) => {
         res.send(req.user)
     })
 
     // Updated user profile
-    app.patch('/user/me', auth, async  (req, res) => {
-        res.send("update my user profile")
+    app.patch('/user/me', auth, async (req, res) => {
+        const updates = Object.keys(req.body)
+        const allowedUpdates = ['name', 'email', 'password', 'age']
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+        if (!isValidOperation) {
+            return res.status(400).send({ error: 'Invalid operation' })
+        }
+
+        try {
+            // loop through all property of user and store data
+            updates.forEach((update) => req.user[update] = req.body[update])
+            await req.user.save()
+
+            res.send(req.user)
+        } catch (e) {
+            res.status(400).send(e)
+        }
     })
 
     // Delete user
-    app.delete('/user/me', auth, async  (req, res) => {
+    app.delete('/user/me', auth, async (req, res) => {
         try {
             await req.user.remove()
             res.send(req.user)
